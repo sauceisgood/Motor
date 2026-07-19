371c60491f8ce2495dfcf3387d4949dd73d52b45
sauceisgood <53635512+sauceisgood@users.noreply.github.com>
2026-07-19 01:06:08 +0100

    Update metadata title and description in layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,9 +1,6 @@
-import type { Metadata } from 'next'
-import './globals.css'
-
-export const metadata: Metadata = {
-  title: 'Szwego & Yupoo Search',
-  description: 'Motor de busca para Szwego e Yupoo',
+export const metadata = {
+  title: 'Motor de Busca',
+  description: 'Pesquisa em Szwego e Yupoo',
 }
 
 export default function RootLayout({
